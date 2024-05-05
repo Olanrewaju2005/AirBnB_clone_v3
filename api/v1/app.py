39e74619b75b46fe90f49123bfb653aa835a6824
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-05-05 12:34:02 +0100

    task5

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,29 @@ Create a flask app; and register the blueprint app_views to flask instance
 """
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """
+
+    """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    custom handler for 404 error
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
