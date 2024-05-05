712d18c311a0e151a1ecf413ceb95dc6221618f4
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2024-05-05 12:17:33 +0100

    task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Create flask app; app_views
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status')
 def api_status():
@@ -12,3 +13,19 @@ def api_status():
     """
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route('/stats')
+def get_status():
+    """
+    Returns number of each object by type
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+    }
+
+    return jsonify(stats)
